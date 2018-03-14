88550cbd745ced466bb6c6752a0d99798f74dde7
Rushanf <rushan.sameera@gmail.com>
2018-03-14 22:28:46 +0530

    Register Page

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/static'));
-app.get('/', function(req, res,next) {  
+app.get('/', function(req, res,next) {     
+    res.sendFile(__dirname + '/Register.html');
+});
+app.get('/index', function(req, res,next) {     
     res.sendFile(__dirname + '/index.html');
 });
 
