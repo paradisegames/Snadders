9fc612f9501325f8e6796d217a668f45f9144afb
EmblaTeam <EmblaTeam@embla.lk>
2018-03-14 11:56:16 +0530

    changed the port var

--- server.js
+++ server.js
@@ -4,29 +4,28 @@ var fs = require('fs')
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html'));
-}).listen(8080, function() {
+}).listen((process.env.PORT || 8080), function() {
     console.log('Listening at: http://localhost:8080');
 });
 
-
 // attach Socket.io to our HTTP server
 io = socketio.listen(server);
-
-var room = '';
 
 // handle incoming connections from clients
 io.sockets.on('connection', function(socket) {
     // once a client has connected, we expect to get a ping from them saying what room they want to join
     socket.on('room', function(room) {
+        console.log(room);
         socket.join(room);
+    });
+    socket.on('message', function (msg) { 
+        console.log(msg);
+        io.sockets.in(room).emit('message', msg); 
     });
 });
 
 // now, it's easy to send a message to just the clients in a given room
-io.sockets.on('message', function (msg) { 
-    console.log(room);
-    io.sockets.in(room).emit('message', msg); 
-});
+
 
 
 
